418f977fd8ca92407f512ddf56f5a7bf9429c222
Keith <krpbarber@gmail.com>
2025-03-29 13:53:08 -0700

    Flexbox practice

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
+// This app displays a series of numbered boxes, ordered as
+// a series of rows containing two columns of two boxes each.
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from "./styles";
+import Box from "./Box";
+import Column from "./Column";
+import Row from "./Row";
+
+// Number of boxes to render. Change this to change how many boxes are on screen.
+const numBoxes = 12;
+
+// Array of numbers that will be printed within each box
+const boxes = new Array(numBoxes).fill(null).map((v, i) => i + 1);
+
+// Array of rows, used to assign keys and index into the boxes array above
+const rows = new Array(Math.ceil(numBoxes / 4)).fill(null).map((v, i) => i);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={false} />
+      {rows.map((i) => (
+        <Row key={i}>
+          <Column>
+            <Box key={4 * i}>#{boxes[4 * i]}</Box>
+            {4 * i + 1 < numBoxes && (
+              <Box key={4 * i + 1}>#{boxes[4 * i + 1]}</Box>
+            )}
+          </Column>
+          <Column>
+            {4 * i + 2 < numBoxes && (
+              <Box key={4 * i + 2}>#{boxes[4 * i + 2]}</Box>
+            )}
+            {4 * i + 3 < numBoxes && (
+              <Box key={4 * i + 3}>#{boxes[4 * i + 3]}</Box>
+            )}
+          </Column>
+        </Row>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,42 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    flexShrink: 1,
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "start",
+    alignItems: "center"
+  },
+  box: {
+    width: 100,
+    maxHeight: 100,
+    height: 100,
+    flexShrink: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray",
+    margin: 10
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
+  },
+});
